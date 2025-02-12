88c644179a98e2a9992b8e3367dac9df2003d573
Remus Poon <remuspoon@gmail.com>
2025-02-12 13:53:46 +0000

    linted homepage

--- app/[user]/(home)/homePage.tsx
+++ app/[user]/(home)/homePage.tsx
@@ -43,55 +43,57 @@ const HomePage = () => {
     }
 
   }, [userData.friend, userData.friendPending])
-
   const renderHomePage = () => {
     switch (homePageState.state) {
       case "noFriend":
         return (
-          <View className="flex-1 bg-secondary items-center justify-center gap-y-10 px-5" >
-        <TouchableOpacity 
-          className="w-2/3 aspect-square rounded-full bg-white items-center justify-center"
-          activeOpacity={.5}
-          disabled={true}>
-            <View className="pt-6 items-center justify-center">
-              <Heart size={150} fill="#9D9D9D" color="#9D9D9D" />
-              <Text className="absolute text-white text-8xl font-f500 pt-8">?</Text>
+          <View className="flex-1 bg-secondary items-center justify-center gap-y-10 px-5">
+            <TouchableOpacity 
+              className="w-2/3 aspect-square rounded-full bg-white items-center justify-center"
+              activeOpacity={0.5}
+              disabled={true}
+            >
+              <View className="pt-6 items-center justify-center">
+                <Heart size={150} fill="#9D9D9D" color="#9D9D9D" />
+                <Text className="absolute text-white text-8xl font-f500 pt-8">?</Text>
+              </View>
+              <View />
+            </TouchableOpacity>
+            <View className="flex-col items-center justify-center gap-y-2">
+              <Text className="text-grey text-4xl text-center font-f500">
+                Tap your profile picture to add your friend!
+              </Text>
             </View>
-            <View>
-              
-            </View>
-          </TouchableOpacity>
-        <View className="flex-col items-center justify-center gap-y-2">
-          <Text className="text-grey text-4xl text-center font-f500">Tap your profile picture to add your friend!</Text>
-        </View>
-      </View>
+          </View>
         )
       case "pending":
         return (
-          <View className="flex-1 bg-secondary items-center justify-center gap-y-10 px-5" >
-        <TouchableOpacity 
-          className="w-2/3 aspect-square rounded-full bg-white items-center justify-center"
-          activeOpacity={.5}
-          disabled={true}>
-            <View className="pt-6 items-center justify-center">
-              <Heart size={150} fill="#9D9D9D" color="#9D9D9D" />
-              <Text className="absolute text-white text-8xl font-f500 pt-8">?</Text>
+          <View className="flex-1 bg-secondary items-center justify-center gap-y-10 px-5">
+            <TouchableOpacity 
+              className="w-2/3 aspect-square rounded-full bg-white items-center justify-center"
+              activeOpacity={0.5}
+              disabled={true}
+            >
+              <View className="pt-6 items-center justify-center">
+                <Heart size={150} fill="#9D9D9D" color="#9D9D9D" />
+                <Text className="absolute text-white text-8xl font-f500 pt-8">?</Text>
+              </View>
+              <View />
+            </TouchableOpacity>
+            <View className="flex-col items-center justify-center gap-y-2">
+              <Text className="text-grey text-4xl text-center font-f500">
+                Waiting for {pendingFriendName} to accept your request!
+              </Text>
             </View>
-            <View>
-              
-            </View>
-          </TouchableOpacity>
-        <View className="flex-col items-center justify-center gap-y-2">
-          <Text className="text-grey text-4xl text-center font-f500">Waiting for {pendingFriendName} to accept your request!</Text>
-        </View>
-      </View>
+          </View>
         )
       case "friend":
         return (
           <View className="flex-1 bg-secondary items-center justify-center gap-y-10 px-5">
             <TouchableOpacity 
               className="w-2/3 aspect-square rounded-full bg-white items-center justify-center"
-              activeOpacity={0.5}>
+              activeOpacity={0.5}
+            >
               <View className="pt-6">
                 <Heart size={150} fill="#FF6C6E" color="#FF6C6E" />
               </View>
